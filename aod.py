51b34fab4a1b0d7cde9fd886ead79afcf6925ffd
ebadawy <ehalbadawy93@gmail.com>
2015-12-19 14:10:58 +0200

    Build a naive forground mask extraction method

--- aod.py
+++ aod.py
@@ -1,6 +1,21 @@
 import numpy as np
 import cv2
 
+def getForegroundMask(frame, background, th):
+    # reduce the nois in the farme
+    frame = cv2.blur(frame, (5,5))
+    # get the absolute difference between the foreground and the background
+    fgmask= cv2.absdiff(frame, background)
+    # convert foreground mask to gray
+    fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+    # apply threshold (th) on the foreground mask
+    _, fgmask = cv2.threshold(fgmask, th, 255, cv2.THRESH_BINARY)
+    # setting up a kernal for morphology
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+    # apply morpholoygy on the foreground mask to get a better result
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
+    return fgmask
+
 cap = cv2.VideoCapture('1.mp4')
 
 # setting up a kernal for morphology
